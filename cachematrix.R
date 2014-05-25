77b61c5f4c84bb4d503b8dda2994f1e5cd458db7
Scott <scott.w.arendt@gmail.com>
2014-05-24 20:16:56 -0500

    nitial cachematrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix
 
-## Write a short comment describing this function
+## This function makes use of the <<- operator to cache matrix data
+## mtrx will be the cached matrix
+## while mtrxinv will contain a cached inverse of mtrx
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mtrx = matrix()) {
+  mtrxinv <<- NULL
+  
+  ## Set the global environment matrix mtrx
+  ## Clear any values in the mtrxinv because the original has changed 
+  setMatrix <- function(m) {
+    mtrx <<- m
+    mtrxinv <<- NULL
+  }
+  
+  ## Retreive the Cached matrix 
+  getMatrix <- function() {
+    mtrx
+  } 
+  
+  ## Otherwise use solve to generate  an inverse matrix
+  setInverseMatrix <- function (m) {
+    mtrxinv <<- solve(m)
 
+  }
+  
+  ## Retreive the cached inverse matrix 
+  getInverseMatrix <- function () {
+    mtrxinv
+  }
+  
+  list (setMatrix = setMatrix, getMatrix = getMatrix, 
+        setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
+  
 }
 
-
+## cacheSolve 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m = matrix()) {
+  ## Return a matrix that is the inverse of 'x'
+  mi <- m$getInverseMatrix()
+  
+  ## If 
+  if (!is.null(mi)) {
+    message("Getting cached data ....")
+            return(mi)
+  }
+  
+  data <- m$getMatrix()
+  
+  m$setInverseMatrix(data)
+  
+  mi <- m$getInverseMatrix()
+ 
+  mi
 }
